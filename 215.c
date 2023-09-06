7ee8d6d451c95a74a42bc57f4890cbdff711287f
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-06 23:21:19 +0800

    Update 215.c

--- 215.c
+++ 215.c
@@ -7,7 +7,7 @@ int main(){
   f -= 8 * cr;
   int r = f / 2 - t;
   int c = s - r - cr;
-  if(c < 0 || r < 0 || cr < 0)
+  if(c < 0 || r < 0 || cr < 0 || f % 2 != 0)
     printf("0");
   else
     printf("%d\n%d\n%d\n", c, r, cr);
