c6ce4d80720f66894bc25bafb76bf4b0c1939489
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-06 23:04:46 +0800

    Create 215.c

---
+++ 215.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int s, f, t;
+  scanf("%d%d%d", &s, &f, &t);
+  int cr = s - t;
+  f -= 8 * cr;
+  int r = f / 2 - t;
+  int c = s - r - cr;
+  if(c < 0 || r < 0 || cr < 0)
+    printf("0");
+  else
+    printf("%d\n%d\n%d\n", c, r, cr);
+  return 0;
+}
