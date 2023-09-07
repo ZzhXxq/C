01199bff5f6ef0ac1731596b3cf07f4f0ef6c989
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-07 23:59:19 +0800

    Update 202.c

--- 202.c
+++ 202.c
@@ -6,10 +6,15 @@ int main(){
   scanf("%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g);
   int newb = (a > 0)? a * c + b : a * c - b;
   int newf = (e > 0)? e * g + f : e * g - f;
+  int top, bottom;
   switch(d){
     case ADD:
-      
+      top = newb * g + newf * c;
+      bottom = c * g;
       break;
   }
+  /*接下來要約分*/
+  /*除出整數part並將分數part轉正*/
+  /*輸出*/
   return 0;
 }
