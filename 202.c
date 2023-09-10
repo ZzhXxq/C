7d3092e4857e886dcc71a860743d7ce68332bcd1
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-10 22:37:50 +0800

    Update 202.c

--- 202.c
+++ 202.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #define ADD 0
+#define SUB 1
+#define MUL 2
+#define DIV 3
 
 int main(){
   int a, b, c, d, e, f, g;
@@ -12,9 +15,40 @@ int main(){
       top = newb * g + newf * c;
       bottom = c * g;
       break;
+    case SUB:
+      top = newb * g - newf * c;
+      bottom = c * g;
+      break;
+    case MUL:
+      top = newb * newf;
+      bottom = c * g;
+      break;
+    case DIV:
+      top = newb * g;
+      bottom = c * newf;
+      if (bottom < 0){
+        top = -top;
+        bottom = -bottom;
+      }
+      break;
+  }
+  int A = (top > 0)? top : -top;
+  int B = bottom;
+  while(A % B != 0){
+    int tmp = A % B;
+    A = B;
+    B = tmp;
   }
   /*接下來要約分*/
+  top /= B;
+  bottom /= B;
   /*除出整數part並將分數part轉正*/
+  int h = top / bottom;
+  int i = (top > 0)? top % bottom : -(top % bottom);
+  int j = bottom;
+  if (i == 0)
+    j = 1;
   /*輸出*/
+  printf("%d\n%d\n%d\n", h, i, j);
   return 0;
 }
