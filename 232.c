84c1e541d3b24a03b5d3931aaee6912e88abe191
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-11 21:49:29 +0800

    Update 232.c

--- 232.c
+++ 232.c
@@ -9,11 +9,30 @@ int main(){
     if (month < 1 || month > 12){
       printf("-1\n");
       continue;
-    }else if (day < 1 || day > 31){
-      printf("-2\n");
-      continue;
-    }  //未完成條件
-    
+    switch (month){
+      case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        if (day < 1 || day > 31){
+          printf("-2\n");
+          continue;
+        }
+      case 4: case 6: case 9: case 11:
+        if (day < 1 || day > 30){
+          printf("-2\n");
+          continue;
+        }
+      case 2:
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+          if (day < 1 || day > 29){
+            printf("-2\n");
+            continue;
+          } else if (day < 1 || day > 28){
+            printf("-2\n");
+            continue;
+          }
+        }
+    }
+
+      
     int sum = day - 1;
     for(int j = 1; j < month; j++){  
       switch (j){
