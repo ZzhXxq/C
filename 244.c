1b3e23844d6a6bb621ab573e21de5f8987e64b51
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-11 23:37:48 +0800

    Create 244.c

---
+++ 244.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int spe_num, fst_num;
+  scanf("%d%d", &spe_num, &fst_num);
+  int sum = 0;
+  while(scanf("%d", num) != EOF){
+    if (num == spe_num)
+      sum += 2000000;
+    else if (num == fst_num)
+      sum += 200000;
+  }
+  
+  return 0;
+}
