8a76df62d422740ab31028eda8c5d9c94bd07eed
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-13 18:40:02 +0800

    Update 97.c

--- 97.c
+++ 97.c
@@ -3,26 +3,31 @@
 int main(){
   int n;
   scanf("%d", &n);
-  int px[n], py[n], cmost[n];
-  for (int i = 0; i < n; i++)
-    scanf("%d%d%d", &px[i], &py[i], cmost[n]);
+  int px[n], py[n], cmost[n], cnow[n];
+  for (int i = 0; i < n; i++){
+    scanf("%d%d%d", &px[i], &py[i], &cmost[i]);
+    cnow[i] = 0;
+  }
   int m, bx, by;
-  int cnow[n] = {0};
   scanf("%d", &m);
   for(int j = 0; j < m; j++){
     scanf("%d%d", &bx, &by);
     int p = 0;
-    int d = (bx[j] - px[0]) + (by[j] - py[0]);
+    while (cnow[p] == cmost[p])
+      p++;
+    int d = (bx > px[p]? bx - px[p] : px[p] - bx);
+    d += (by > py[p]? by - py[p] : py[p] - by);
     for (int i = 0; i < n; i++){
-      int dnew = (bx[j] > px[i]? bx[j] - px[i] : px[i] - bx[j]);
-      dnew += (by[j] > py[i]? by[j] - py[i] : py[i] - by[j]);
+      int dnew = (bx > px[i]? bx - px[i] : px[i] - bx);
+      dnew += (by > py[i]? by - py[i] : py[i] - by);
       if(d > dnew && cnow[i] < cmost[i]){
         d = dnew;
         p = i;
       }  
     }
-    cnow[i]++;
+    cnow[p]++;
   }
-    
+  for (int i = 0; i < n; i++)
+    printf("%d\n", cnow[i]);  
   return 0;
 }
