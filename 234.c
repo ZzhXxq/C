e1f2b1795bdc42f3e44df9a06dd1d385ea6b8b95
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-15 00:16:56 +0800

    Update 234.c

--- 234.c
+++ 234.c
@@ -4,15 +4,7 @@ int main(){
   int a, b, c;
   scanf("%d%d%d", &a, &b, &c);
   int d, e, f;
-  int i = -100000;
-  while (i *(a - i) + c / i != b)
-    i ++;
-  d = i;
-  while(c / (d * i) != a - d - i)
-    i++;
-  f = i;
-  e = a - d - f;  
-  
+    
   printf("%d %d %d", d, e, f);
   return 0;
 }
