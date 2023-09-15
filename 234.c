05d1a07e1446feece7c8a489b4445f9c08549913
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-15 17:30:55 +0800

    Update 234.c

--- 234.c
+++ 234.c
@@ -15,6 +15,19 @@ int main(){
       break;}
   e = -i;
   d = a - e - f;
+  if (d * e * f != c){
+    for (i = 1; i <= 100000; i++){
+      if (i * i * i + a * i * i + b * i + c == 0)
+        break;
+    }
+    f = -i;
+    for (i ; i <= 100000; i++){
+      if (i * i  + (a - f) * i  + b - a * f + f * f == 0)
+        break;
+    }
+    e = -i;
+    d = a - e - f;
+  }
   printf("%d %d %d", d, e, f); 
   return 0;
 }
