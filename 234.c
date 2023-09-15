318d521034841a015be970b82ea2ef7552c9c5b3
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-15 16:56:31 +0800

    Update 234.c

--- 234.c
+++ 234.c
@@ -4,7 +4,17 @@ int main(){
   int a, b, c;
   scanf("%d%d%d", &a, &b, &c);
   int d, e, f;
-    
-  printf("%d %d %d", d, e, f);
+  int i;
+  for (i = -100000; i <= 100000; i++){
+    if (i * i * i + a * i * i + b * i + c == 0)
+      break;
+  }
+  f = -i;
+  for (i ; i <= 100000; i++){
+    if (i * i  + (a - f) * i  + b - a * f + f * f == 0)
+      break;}
+  e = -i;
+  d = a - e - f;
+  printf("%d %d %d", d, e, f); 
   return 0;
 }
