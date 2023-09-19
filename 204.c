a5a1ce65e34ea0f2dc73e7c4f6cf4bd493c6fe11
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-20 01:34:27 +0800

    Update 204.c

--- 204.c
+++ 204.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
   int W, D;
   scanf("%d%d", &W, &D);
-  int mirrow[W][D];
-  
+  bool mirrow[W][D];
+  for (int d = D - 1; d >= 0; d--)
+    for (int w = 0; w < W; w++)
+      scanf("%d", &mirrow[w][d]);
   return 0;
 }
