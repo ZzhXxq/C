8d3ce135266ed2755184c94d3eb015c9fe3b411f
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-19 09:19:18 +0800

    Update 14.c

--- 14.c
+++ 14.c
@@ -7,7 +7,12 @@ int main(){
   int num[ARRAYSIZE] = {0};
   for (int i = 0; i < n; i++)
     scanf("%d", &num[i]);
-  for (int i = n - 1; i >= 0; i--)
-    printf("%d ", num[i]);
+  for (int i = n - 1; i >= 0; i--){
+    if (i != 0)
+      printf("%d ", num[i]);
+    else
+      printf("%d", num[i]);
+  }
+    
   return 0;
 }
