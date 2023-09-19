97343f0b14aaf26c2b16c6be425950be10599163
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-19 08:11:03 +0800

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+  int M, N, x1, y1, E1, N1, F1, x2, y2, E2, N2, F2;
+  scanf("%d%d%d%d%d%d%d%d%d%d%d%d", &M, &N, &x1, &y1, &E1, &N1, &F1, &x2, &y2, &E2, &N2, &F2);
+  
+  return 0;
+}
