8a3471671fa22ec942d5850dcabe6b05488b3aa7
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-20 01:20:38 +0800

    Update 74.c

--- 74.c
+++ 74.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#define mSIZE 200000
 int main(){
   int n;
   scanf("%d", &n);
@@ -8,13 +9,24 @@ int main(){
     scanf("%d", &num[i]);
   int m;
   scanf("%d", &m);
-  for (int j = 0; j < m; j++){
+  int count[mSIZE] = {0};
+  for (int i = 0; i < n; i++){
+    bool noFound = true;
+    for (int j = 0; j < m && noFound; j++)
+      if (num[i] % m == j){
+        count[j]++;
+        noFound = false;
+      }
+  }
+  for (int j = 0; j < m; j++)
+    printf("%d\n", count[j]);
+  /*for (int j = 0; j < m; j++){
     int count = 0;
     for (int i = 0; i < n; i++){
       if (num[i] % m == j)
         count++;
     }
     printf("%d\n", count);
-  }
+  }*/
   return 0;
 }
