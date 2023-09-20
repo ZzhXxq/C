153c6487583e148dc9d5338a7ea8205480a6c4e2
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-20 21:46:54 +0800

    Update 204.c

--- 204.c
+++ 204.c
@@ -6,7 +6,10 @@ int main(){
   scanf("%d%d", &W, &D);
   bool mirrow[W][D];
   for (int d = D - 1; d >= 0; d--)
-    for (int w = 0; w < W; w++)
-      scanf("%d", &mirrow[w][d]);
+    for (int w = 0; w < W; w++){
+      int tmp;
+      scanf("%d", &tmp);
+      mirrow[w][d] = (tmp == 1);
+    }
   return 0;
 }
