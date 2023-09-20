67408d2c196be6f021f947432e3679517629d72b
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-20 23:55:09 +0800

    Update 195.c

--- 195.c
+++ 195.c
@@ -8,6 +8,16 @@ int main(){
   int x[n], y[n];
   for (int i = 0; i < n; i++)
     scanf("%d%d", x[i], y[i]);
+  int N = 1;
+  for (int i = 0; i < n; i++){
+    if (x[i] >= 3 && x[i] < 0 && y[i] >= 3 && y[i] < 0 || map[x[i]][y[i]] != 0)
+      continue;
+    if (count % 2 == 1)
+      map[x[i]][y[i]] = 1;
+    } else 
+      map[x[i]][y[i]] = 2;
+      N++;
+  }
   
   return 0;
 }
