01c700fcd474386e39b37be38ece1aa63bde1ef6
ZzhXxq <143996373+ZzhXxq@users.noreply.github.com>
2023-09-26 09:52:17 +0800

    Update 32.c

--- 32.c
+++ 32.c
@@ -7,8 +7,15 @@ int main(){
     arr[n] = x;
     n++;
   }
+  int left, right, lengh;
   for (int i = 0; i <= n; i++){
     if(arr[i] == arr[i+2]){
+      for (int j = 0; ;j++)
+        if (i + j <= n && i - j >= 0 && arr[i-j] == arr[i+2+j]){
+          left = i - j;
+          right = i + 2 + j;
+        }
+      lenth = right - left + 1;
       
     }
     if(arr[i] == arr[i+1]){
